b6dc86a294ea194c8cfed9b3758ba1fa1940a96c
Stein-N <nico-stein-98@outlook.de>
2024-07-07 19:16:26 +0200

    common: fixing ShapedRecipeMixin to only get called when in Crafting table

--- common/src/main/java/net/xstopho/resource_backpacks/mixin/ShapedRecipeMixin.java
+++ common/src/main/java/net/xstopho/resource_backpacks/mixin/ShapedRecipeMixin.java
@@ -1,8 +1,10 @@
 package net.xstopho.resource_backpacks.mixin;
 
 import net.minecraft.core.HolderLookup;
+import net.minecraft.core.NonNullList;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.CraftingInput;
+import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.item.crafting.ShapedRecipe;
 import net.xstopho.resource_backpacks.components.BackpackContainerContent;
 import net.xstopho.resource_backpacks.item.BackpackItem;
@@ -19,17 +21,22 @@ public abstract class ShapedRecipeMixin {
     @Shadow
     public abstract ItemStack getResultItem(HolderLookup.Provider registries);
 
+    @Shadow
+    public abstract NonNullList<Ingredient> getIngredients();
+
     @Inject(method = "assemble", at = @At("HEAD"), cancellable = true)
     private void onAssemble(CraftingInput input, HolderLookup.Provider registries, CallbackInfoReturnable<ItemStack> cir) {
-        ItemStack oldBackpack = input.getItem(4);
+        if (getIngredients().size() > 4) {
+            ItemStack oldBackpack = input.getItem(4);
 
-        if (oldBackpack.getItem() instanceof BackpackItem) {
-            ItemStack newBackpack = this.getResultItem(registries).copy();
+            if (oldBackpack.getItem() instanceof BackpackItem) {
+                ItemStack newBackpack = this.getResultItem(registries).copy();
 
-            if (newBackpack.getItem() instanceof BackpackItem) {
-                BackpackContainerContent container = oldBackpack.get(DataComponentsRegistry.BACKPACK_CONTAINER.get());
-                newBackpack.set(DataComponentsRegistry.BACKPACK_CONTAINER.get(), container);
-                cir.setReturnValue(newBackpack);
+                if (newBackpack.getItem() instanceof BackpackItem) {
+                    BackpackContainerContent container = oldBackpack.get(DataComponentsRegistry.BACKPACK_CONTAINER.get());
+                    newBackpack.set(DataComponentsRegistry.BACKPACK_CONTAINER.get(), container);
+                    cir.setReturnValue(newBackpack);
+                }
             }
         }
     }
