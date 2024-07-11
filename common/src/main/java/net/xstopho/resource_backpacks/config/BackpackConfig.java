da1dd2f1524bb3540d5997a68e07f87e56511d8f
Stein-N <nico-stein-98@outlook.de>
2024-07-11 19:41:17 +0200

    common: remove useless config settings

--- common/src/main/java/net/xstopho/resource_backpacks/config/BackpackConfig.java
+++ common/src/main/java/net/xstopho/resource_backpacks/config/BackpackConfig.java
@@ -10,22 +10,8 @@ public class BackpackConfig {
 
     public static Supplier<Integer> LEATHER_ROWS, LEATHER_COLUMNS, COPPER_ROWS, COPPER_COLUMNS, GOLD_ROWS, GOLD_COLUMNS,
                                     IRON_ROWS, IRON_COLUMNS, DIAMOND_ROWS, DIAMOND_COLUMNS, NETHERITE_ROWS, NETHERITE_COLUMNS;
-    public static Supplier<Boolean> ALLOW_CHESTSLOT, ENABLE_BACKPACK_KEYBIND, OPEN_BACKPACK_FROM_INVENTORY;
 
     static {
-        BUILDER.push("General");
-        ALLOW_CHESTSLOT = BUILDER.comment("Enable the ability to equip the Backpack to your Chestslot.")
-                .comment("When a Server set this to false, your local setting is ignored!")
-                .define("enable_chestslot", true);
-        ENABLE_BACKPACK_KEYBIND = BUILDER.comment("Backpacks can be opened via a keybind.")
-                .comment("When set to false the allow_chestslot setting gets overwritten.")
-                .comment("Default key is B, but can be changed in the Controls Settings.")
-                .define("enable_backpack_keybind", true);
-        OPEN_BACKPACK_FROM_INVENTORY = BUILDER.comment("The first Backpack that is found in your inventory will be opened via the keybind.")
-                .comment("Search order -> inventory -> hotbar")
-                .define("open_backpack_from_inventory", false);
-        BUILDER.pop();
-
         BUILDER.comment("Change how big the backpack inventory is.")
                 .comment("Rows: 1 up to 25")
                 .comment("Columns: 9 up to 50")
@@ -55,9 +41,5 @@ public class BackpackConfig {
         NETHERITE_ROWS = BUILDER.define("rows", 7);
         NETHERITE_COLUMNS = BUILDER.define("columns", 13);
         BUILDER.pop();
-    }
-
-    public static boolean isChestEquipable() {
-        return ALLOW_CHESTSLOT.get() && ENABLE_BACKPACK_KEYBIND.get();
     }
 }
