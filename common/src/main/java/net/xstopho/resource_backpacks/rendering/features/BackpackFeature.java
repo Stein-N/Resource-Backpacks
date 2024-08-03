a2c3425c38b62ffe193a6632766946725ac9d258
Stein-N <nico-stein-98@outlook.de>
2024-08-03 14:52:01 +0200

    common: BackpackFeature also uses the BackpackRenderer

--- common/src/main/java/net/xstopho/resource_backpacks/rendering/features/BackpackFeature.java
+++ common/src/main/java/net/xstopho/resource_backpacks/rendering/features/BackpackFeature.java
@@ -1,19 +1,16 @@
 package net.xstopho.resource_backpacks.rendering.features;
 
 import com.mojang.blaze3d.vertex.PoseStack;
-import com.mojang.math.Axis;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.model.PlayerModel;
 import net.minecraft.client.player.AbstractClientPlayer;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.RenderLayerParent;
 import net.minecraft.client.renderer.entity.layers.RenderLayer;
-import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemDisplayContext;
 import net.minecraft.world.item.ItemStack;
 import net.xstopho.resource_backpacks.item.BackpackItem;
+import net.xstopho.resource_backpacks.rendering.BackpackRenderer;
 
 public class BackpackFeature extends RenderLayer<AbstractClientPlayer, PlayerModel<AbstractClientPlayer>> {
 
@@ -29,16 +26,7 @@ public class BackpackFeature extends RenderLayer<AbstractClientPlayer, PlayerMod
         Item chestItem = chestSlot.getItem();
 
         if (chestItem instanceof BackpackItem) {
-            poseStack.pushPose();
-            poseStack.mulPose(Axis.XP.rotationDegrees(180));
-            poseStack.translate(0, -0.29, -0.25);
-
-            if (player.isShiftKeyDown()) {
-                poseStack.mulPose(Axis.XP.rotationDegrees(29));
-                poseStack.translate(0, -0.1, -0.072);
-            }
-            Minecraft.getInstance().getItemRenderer().renderStatic(chestSlot, ItemDisplayContext.FIXED, light, OverlayTexture.NO_OVERLAY, poseStack, multiBufferSource, player.clientLevel, 0);
-            poseStack.popPose();
+            BackpackRenderer.render(chestSlot, poseStack, multiBufferSource, light, player);
         }
     }
 }
